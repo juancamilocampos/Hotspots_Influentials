359f2e2c690decea36f1406d3c6f158d4284cb6f
jccampos <jcccampos@javerianacali.edu.co>
2017-01-05 19:19:01 -0500

    Initial commit

---
+++ Logic/Utils/Graph Retweets.py
@@ -0,0 +1,51 @@
+from pymongo import MongoClient
+import networkx as nx
+from Logic.Utils.config import Configuration
+from collections import Counter
+
+
+class GraphRetweets:
+    """ Networkx allows to use a list of repeated elements as nodes
+    it will not repeat the node in the node list, instead it will just
+    create a unique node"""
+
+    def __init__(self):
+        self.ConfigApi = Configuration()
+        self.client = MongoClient(self.ConfigApi.MONGO_URL())
+        self.db = self.client['local']
+        self.coll = self.db[self.ConfigApi.MONGO_COLL_TweetsGnip()]
+        self.nodes = list()
+        self.edges = list()
+        self.edges_weight = list()
+        self.undirected_edges_weight = list()
+        self.graphdi = nx.DiGraph()
+        self.graph = nx.Graph()
+
+    def create(self):
+        cursor = self.coll.find({"object.objectType": 'activity'},
+                                {"actor.id": 1, "actor.friendsCount": 1, "actor.followersCount": 1,
+                                 "object.actor.id": 1,
+                                 "object.actor.friendsCount": 1, "object.actor.followersCount": 1})
+
+        # sorted is used to obtain the total weight in a direct graph
+        for tweet in cursor:
+            self.edges.append((tweet["actor"]["id"], tweet["object"]["actor"]["id"]))
+            self.nodes.append((tweet["actor"]["id"], {'friends': tweet["actor"]["friendsCount"],
+                                                      'followers': tweet["actor"]["followersCount"]}))
+            self.nodes.append((tweet["object"]["actor"]["id"], {'friends': tweet["object"]["actor"]["friendsCount"],
+                                                                'followers': tweet["object"]["actor"][
+                                                                    "followersCount"]}))
+
+        self.edges_weight = Counter(self.edges).most_common()
+        self.edges_weight = [(s[0][0], s[0][1], s[1]) for s in self.edges_weight]
+
+        self.graphdi.add_nodes_from(self.nodes)
+        self.graphdi.add_weighted_edges_from(self.edges_weight)
+
+        self.edges = [sorted(s) for s in self.edges]
+        self.undirected_edges_weight = Counter(self.edges).most_common()
+        self.undirected_edges_weight = [(s[0][0], s[0][1], s[1]) for s in self.edges_weight]
+        self.graph.add_nodes_from(self.nodes)
+        self.graph.add_weighted_edges_from(self.edges_weight)
+
+        return [self.graphdi, self.graph]
---
+++ Logic/Utils/config.py
@@ -0,0 +1,81 @@
+class Configuration:
+    """
+    Define the credentials needed to establish a connection with twitter and do streaming.
+    https://apps.twitter.com/
+    """
+
+    def __init__(self):
+        self.Version = 0.1
+        self.Author = 'CAOBA_Extraccion'
+
+    #### Variables Twitter
+    def TWITTER_CONSUMER_KEY(self):
+        """
+
+        :rtype: String
+        :returns:Consumer key to allow the streaming from twitter.
+        """
+        return 'r8g1ylfHtqmdD09GJQwBWEuen'
+
+    def TWITTER_CONSUMER_SECRET(self):
+        """
+
+        :rtype: String
+        :returns:Consumer secret to allow the streaming from twitter.
+        """
+        return 'OlbOiGAs5i2MLNrlXy8tEWYby4O8m8YbSzsUd62KXJbNqhxNJp'
+
+    def TWITTER_OAUTH_TOKEN(self):
+        """
+
+        :rtype: String
+        :returns:Authentication token to allow the streaming from twitter.
+        """
+        return '563154603-BMOkX4yBnCUNuYT7UQFvqm7ixvBptba8z8CacqNZ'
+
+    def TWITTER_OAUTH_TOKEN_SECRET(self):
+        """
+
+        :rtype: String
+        :returns:Authentication token secret to allow the streaming from twitter.
+        """
+        return 'V7f1HjgTnlEH3Wl0XP0XEwlKohxAi8yeYI1Yhl9fwgJNf'
+
+    ### MongoDB Variables
+
+    def MONGO_URL(self):
+        """
+
+        :rtype: String
+        :returns:mongodb server direction
+        """
+
+        return 'mongodb://localhost:27017//'
+
+    def MONGO_DB(self):
+        """
+
+        :rtype: String
+        :returns:String: name of the data base to be created on mongo
+        """
+        return 'Caoba'
+
+    def MONGO_COLL_TWEET(self):
+        """
+
+        :rtype: String
+        :returns:name of the collection where the Twitter data will be saved
+        """
+        return 'Tweets'
+
+    def MONGO_COLL_TweetsGnip(self):
+        return "tweetsGNIP25"
+
+    def MONGO_COLL_LOGICCONVERSATION(self):
+        """
+
+        :rtype: String
+        :returns:String::name of the collection where the processing data will be saved
+        """
+
+        return 'UserGeneratedContent'
