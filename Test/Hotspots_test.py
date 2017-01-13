cacda90ec5fe772c8b52d736c2ce8a0a02ab8f25
jccampos <jcccampos@javerianacali.edu.co>
2017-01-13 14:04:29 -0500

    Second version

--- Test/Hotspots_test.py
+++ Test/Hotspots_test.py
@@ -7,28 +7,47 @@ graph_class = GraphRetweets()
 
 inf = gv.influentials(graph_directed)
 
-print(len(inf))
-
 edges_weight = list()
 [edges_weight.append(edata['weight']) for u, v, edata in graph_undirected.edges(data=True) if 'weight' in edata]
 
-f = open('weights-27.txt', 'w')
+f = open('weights-25.txt', 'w')
 f.write('{')
 for item in edges_weight:
     f.write("%s " % item)
     f.write(",")
-
 f.write("0")
 f.write('}')
 f.close()
 
-# g = max(nx.connected_component_subgraphs(graph_undirected), key=len)  # To get the largest connected component
+g = max(nx.connected_component_subgraphs(graph_undirected), key=len)  # To get the largest connected component
+K = list(set(inf).intersection(g.nodes()))
+
+g, tag = gv.parallel_dijkstra(g, K, 1)
+Du = nx.get_node_attributes(g, 'Dv')
+Vu = nx.get_node_attributes(g, 'V')
+
+f = open('Dv.txt', 'w')
+f.write('{')
+for item in list(Du.values()):
+    f.write("%s " % item)
+    f.write(",")
+f.write("0")
+f.write('}')
+f.close()
+
+results = gv.groups(Vu)
+print(len(results['id:twitter.com:100293263']))
+
+# # # [hotspot_boolean, c, final_hotspots] = gv.graph_hotspots(results, K, g, 1.01)
+# #
+# [hotspot_boolean, c, weights_influentials] = gv.graph_hotspots(results, K, g, 1.01)
+# print(1)
+# f = open('weights-between-influentials-27.txt', 'w')
+# f.write('{')
+# for item in weights_influentials:
+#     f.write("%s " % item)
+#     f.write(",")
 #
-# K = list(set(inf).intersection(g.nodes()))
-# g, tag = gv.parallel_dijkstra(g, K, 1)
-# Du = nx.get_node_attributes(g, 'Dv')
-# Vu = nx.get_node_attributes(g, 'V')
-#
-# results = gv.groups(nx.get_node_attributes(g, 'V'))
-#
-# [hotspot_boolean, c, final_hotspots] = gv.graph_hotspots(results, K, g, 1.01)
+# f.write("0")
+# f.write('}')
+# f.close()
